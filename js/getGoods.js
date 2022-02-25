66d3a7e296199df7e4523b4c59e6aa208062b70d
TonyArty <i.openuser@gmail.com>
2022-02-26 01:16:17 +0500

    getData firebase res data, links forEach

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,11 +1,19 @@
 const getGoods = () => {
-  fetch('/db/db.json')
-    .then(function(response){
-      return response.json()
-    })
-    .then(function(data){
-      console.log(data);
-    })
+
+  const links = document.querySelectorAll('.navigation-link');
+  links.forEach(function(item){
+    console.log(item);
+  })
+
+  const getData = () => {
+    fetch('https://willberries-glo-default-rtdb.firebaseio.com/db.json')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+  }
 
 }
 getGoods()
+
+// 16:37
