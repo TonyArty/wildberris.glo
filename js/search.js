0af5e003243c2af1210abf13b8fc6e9b644e464b
TonyArty <i.openuser@gmail.com>
2022-02-25 15:46:16 +0500

    searchBtn click input.value

--- js/search.js
+++ js/search.js
@@ -1,10 +1,9 @@
 const search = function() {
-  const searchBlock = document.querySelector('.search-block');
   const input = document.querySelector('.search-block > input');
-  const button = document.querySelector('.search-block > button');
-  
-  input.addEventListener('input', (event) => {
-    console.log(event.target.value);
+  const searchBtn = document.querySelector('.search-block > button');
+
+  searchBtn.addEventListener('click', () => {
+    console.log(input.value);
   })
 }
 search()
