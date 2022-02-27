2aa1839f53eb57c75e7ca672096d344d7a820813
TonyArty <i.openuser@gmail.com>
2022-02-27 16:31:18 +0500

    data.filter item.gender localStorage, linkValue

--- js/getGoods.js
+++ js/getGoods.js
@@ -6,28 +6,28 @@ const getGoods = () => {
     fetch('https://willberries-glo-default-rtdb.firebaseio.com/db.json')
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        const array = data.filter(function(item){
+          return item.gender === 'Mens'
+        })
+        localStorage.setItem('goods', JSON.stringify(array));
       })
   }
 
   links.forEach((link) => {
     link.addEventListener('click', (event) => {
       event.preventDefault();
+      const linkValue = link.textContent;
+      console.log(linkValue);
       getData();
     })
   })
 
-  localStorage.setItem('goods', JSON.stringify([1,2,3,4,5]))
-  //JSON.stringify({ name: 'all' })
-
-  const goods = JSON.parse(localStorage.getItem('goods'));
-  console.log(goods);
-
-  console.log(localStorage);
-
-  localStorage.removeItem('goods');
-
-  console.log(localStorage);
+  // localStorage.setItem('goods', JSON.stringify({ name: 'all' }))
+  // const goods = JSON.parse(localStorage.getItem('goods'));
+  // console.log(goods);
+  // console.log(localStorage);
+  // localStorage.removeItem('goods');
+  // console.log(localStorage);
 
 }
 getGoods()
