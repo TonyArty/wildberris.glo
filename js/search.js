07a685fe75ba24fd478e4c52926098746af4b483
TonyArty <i.openuser@gmail.com>
2022-03-07 13:38:08 +0500

    try catch

--- js/search.js
+++ js/search.js
@@ -2,8 +2,14 @@ const search = function() {
   const input = document.querySelector('.search-block > input');
   const searchBtn = document.querySelector('.search-block > button');
 
-  searchBtn.addEventListener('click', () => {
-    console.log(input.value);
-  })
+
+
+  try {
+    searchBtn.addEventListener('click', () => {
+      console.log(input.value);
+    })
+  } catch(e) {
+    console.error(e.message);
+  }
 }
 search()
